b8b613aa9d6179d2d627115340bd9935a60d179e
hambalee <18182920+hambalee@users.noreply.github.com>
2023-12-09 11:39:24 +0700

    Tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,6 +16,30 @@ class Post < ApplicationRecord
       errors.add(:body, "must have more than 10 characters")
     end
   end
+
+  # attr_accessor :tags_string
+
+  def tags_string
+    # tags.map(&:name).join(', ')
+    tags.pluck(:name).join(', ')
+  end
+
+  def tags_string=(value)
+    # value = "game, movie"
+    words = value.split(',').map{|s| s.strip.downcase}
+    # ['game', 'movie']
+
+    #
+    existing_tags = tags.pluck(:name)
+    words = words - existing_tags
+    words.each do |word|
+      # tag = Tag.new(name: word)
+      # tag.save
+    tag = Tag.find_or_create_by(name: word)
+
+    tags << tag
+    end
+  end
 end
 
 
@@ -26,3 +50,8 @@ end
 # bundle exec rails g model tag name:string
 # bundle exec rails g model post_tag tag:references post:references
 # bundle exec rails db:migrate
+
+# Post.joins(:writer).where('writers.id' => 2)
+# Post.joins(:writer).group('writers.id')
+# Post.joins(:tags).group('tags.name')
+# Post.joins(:tags).group('tags.name').count
